09755ca3763579c31a784e03105eca4bef0d69b5
Oya-Tomo <82521012+Oya-Tomo@users.noreply.github.com>
2025-03-31 13:21:17 +0900

    update: add document schema for generation

--- schema.py
+++ schema.py
@@ -10,19 +10,61 @@ class ElementSchema(BaseModel, extra="forbid"):
     content: str = Field(
         ...,
         description="Content of the element. "
-        "(text: markdown text that only includes plain/bold/italic text, "
+        "(text: plain text (includes markdown bold/italic/strike span), "
         "table: csv, "
         "katex: latex formula)",
     )
+
+    def to_markdown(self) -> str:
+        if self.type == "text":
+            return self.content
+        elif self.type == "table":
+            return f"```csv\n{self.content}\n```"
+        elif self.type == "katex":
+            return f"$${self.content}$$"
+        else:
+            raise ValueError(f"Unknown element type: {self.type}")
+
+
+class SubsectionSchema(BaseModel, extra="forbid"):
+    title: str = Field(..., description="Title of the subsection")
+    contents: list[ElementSchema] = Field(
+        ..., description="List of subsection contents"
+    )
+
+    def to_markdown(self) -> str:
+        return f"### {self.title}\n" + "\n".join(
+            [content.to_markdown() for content in self.contents]
+        )
 
 
 class SectionSchema(BaseModel, extra="forbid"):
     title: str = Field(..., description="Title of the section")
     contents: list[ElementSchema] = Field(..., description="List of section contents")
+    subsections: list[SubsectionSchema] = Field(
+        ..., description="List of subsections in the section"
+    )
+
+    def to_markdown(self) -> str:
+        return (
+            f"## {self.title}\n"
+            + "\n".join([content.to_markdown() for content in self.contents])
+            + "\n\n"
+            + "\n\n".join([subsection.to_markdown() for subsection in self.subsections])
+        )
 
 
 class DocumentSchema(BaseModel, extra="forbid"):
-    sections: list[SectionSchema] = Field(..., description="List of document contents")
+    sections: list[SectionSchema] = Field(
+        ..., description="List of sections in the document"
+    )
+
+    def to_markdown(self) -> str:
+        return "\n\n".join([section.to_markdown() for section in self.sections])
+
+
+class KeywordsSchema(BaseModel, extra="forbid"):
+    keywords: list[str] = Field(..., description="List of keywords in the document")
 
 
 if __name__ == "__main__":
--- summary.py
+++ summary.py
@@ -4,9 +4,10 @@ import json
 import pprint
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
-from schema import DocumentSchema, SectionSchema
+from schema import SectionSchema, KeywordsSchema
 
 client = OpenAI()
 client.api_key = os.getenv("OPENAI_API_KEY")
@@ -23,28 +24,23 @@ def file_analyze(system_prompt, user_prompt, json_schema) -> None:
                         "type": "text",
                         "text": system_prompt,
                     }
-                ]
+                ],
             },
             {
                 "role": "user",
                 "content": [
                     {
                         "type": "file",
-                        "file": {
-                            "file_id": "file-RinaaMjubEnqi4dVP7gCNs"
-                        }
+                        "file": {"file_id": "file-RinaaMjubEnqi4dVP7gCNs"},
                     },
                     {
                         "type": "text",
                         "text": user_prompt,
-                    }
-                ]
-            }
+                    },
+                ],
+            },
         ],
-        response_format={
-            "type": "json_schema",
-            "json_schema": json_schema
-        }
+        response_format={"type": "json_schema", "json_schema": json_schema},
     )
 
     return json.loads(completion.choices[0].message.content)
@@ -69,6 +65,11 @@ user_prompt = """\
 この論文の概要について教えてください。
 このセクションは「概要」セクションに相当します。
 ここでは構造的な文章よりも、論文全体の流れをつかめるような要約をしてください。
+
+# Response format: Use given Json schema
+section.title: 概要
+section.contents: 論文中の「abstract/summary/background」の要約
+section.subsections: empty
 """
 
 json_schema = {
@@ -78,5 +79,103 @@ json_schema = {
 }
 
 result = file_analyze(system_prompt, user_prompt, json_schema)
-
 pprint.pprint(result)
+
+user_prompt = """\
+# Task: Generate summary from Thesis/Paper/Article
+このセクションは「手法」セクションに相当します。
+論文が提案している概念・手法・実施した実験について、論文の論述の流れを基に詳しく教えてください。
+読みやすいように、構造的な文章で要約してください。
+
+# Output format: Use given Json schema
+section.title: 手法
+section.contents: 論文中の「手法」の概要
+section.subsections: 論文中の「手法」の各サブセクションの概要
+section.subsections[].title: サブセクションのタイトル
+section.subsections[].contents: サブセクションの概要
+"""
+
+json_schema = {
+    "name": "document_schema",
+    "strict": True,
+    "schema": SectionSchema.model_json_schema(),
+}
+
+result = file_analyze(system_prompt, user_prompt, json_schema)
+pprint.pprint(result)
+
+user_prompt = """\
+# Task: Generate summary from Thesis/Paper/Article
+このセクションは「新規性・差分」セクションに相当します。
+論文中で述べられる既存手法との違いや提案手法のメリット・デメリットを基に詳しくまとめてください。
+読みやすいように、構造的な文章で要約してください。
+
+# Output format: Use given Json schema
+section.title: 新規性・差分
+section.contents: 論文中の「新規性・差分」の概要
+section.subsections: 「新規性・差分」の各サブセクション
+section.subsections[].title: サブセクションのタイトル
+section.subsections[].contents: サブセクションの概要
+"""
+
+json_schema = {
+    "name": "document_schema",
+    "strict": True,
+    "schema": SectionSchema.model_json_schema(),
+}
+
+result = file_analyze(system_prompt, user_prompt, json_schema)
+pprint.pprint(result)
+
+user_prompt = """\
+# Task: Generate summary from Thesis/Paper/Article
+このセクションは「結果」セクションに相当します。
+論文中で述べられる実験結果や評価結果を基に詳しくまとめてください。
+特に、筆者が強調しているポイントや、実験結果の解釈についても詳しく教えてください。
+読みやすいように、構造的な文章で要約してください。
+
+# Output format: Use given Json schema
+section.title: 結果
+section.contents: 論文中の「結果」の概要
+section.subsections: 「結果」の各サブセクション
+section.subsections[].title: サブセクションのタイトル
+section.subsections[].contents: サブセクションの概要
+"""
+
+json_schema = {
+    "name": "document_schema",
+    "strict": True,
+    "schema": SectionSchema.model_json_schema(),
+}
+
+result = file_analyze(system_prompt, user_prompt, json_schema)
+pprint.pprint(result)
+
+system_prompt = """\
+# Task: Thesis/Paper/Article analysis
+論文の添付ファイルを基に、ユーザーの命令に従って論文から情報を要約・抽出してください。
+"""
+
+user_prompt = """\
+# Task: Extract keywords or topics from Thesis/Paper/Article
+この論文のキーワードやトピックを抽出してください。
+
+# # Output format: Use given Json schema
+{
+    "keywords": [
+        "keyword1",
+        "keyword2",
+        ...
+    ]
+}
+"""
+
+json_schema = {
+    "name": "document_schema",
+    "strict": True,
+    "schema": KeywordsSchema.model_json_schema(),
+}
+
+result = file_analyze(system_prompt, user_prompt, json_schema)
+pprint.pprint(result)
+print(KeywordsSchema.model_validate(result))
